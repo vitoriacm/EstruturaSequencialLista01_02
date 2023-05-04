1dc1fd834cc0408aa7bba2bd0f40a920bf12af6f
vitoriacm <anavitoria.cezar@gmail.com>
2023-05-04 14:59:40 -0300

    Primeiro Commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+    
+        int numero;
+
+        Scanner teclado = new Scanner (System.in); 
+        numero = teclado.nextInt();
+
+        System.out.println(" o numero informado foi [ "+ numero+"]");
+
+    }
+}
